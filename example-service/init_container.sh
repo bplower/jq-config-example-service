1b8d563f7bea78cd823cac6fc948f90ccfe1f413
Brahm Lower <bplower@gmail.com>
2020-04-10 02:07:10 -0700

    better jq script for null validation check

--- example-service/init_container.sh
+++ example-service/init_container.sh
@@ -6,19 +6,15 @@
 set -e
 SETTINGS_PATH=/settings.json
 
-# This is a jq script. It searches through a given structure for any null
-# values. It builds the key path (like ".my.foo.bar") and prints a human
-# readable warning for each occurence. Finally, it asserts that the list of
-# nulls should be zero in length. This will report 'false' when there are
-# errors, which causes jq to exit with a non-zero exit code when run with the
-# '--exit-status' flag
-NULL_CHECK_SCRIPT='[ path(..|select(type=="null")) | join(".") ] | (.[] | "Missing value for config: .\(.)"), length == 0'
+# This is a jq script that searches through a given structure for null values
+# and prints a human readable error for each occurence. Causes jq to exit with
+# status code 5 if there are errors
+NULL_CHECK_SCRIPT='[ path(..|select(type=="null")) | "Missing value for config: .\( join(".") )\n" ] | if length > 0 then (join("") | halt_error) else empty end'
 
 # Process the settings template file (the *.jq file), writing the result to
-# the expected setting path. The resulting content is also piped into a
-# validation script, which checks for null values, returning a non-zero exit
-# code if any are found.
-jq --null-input --from-file /settings.json.jq | tee $SETTINGS_PATH | jq --exit-status --raw-output "$NULL_CHECK_SCRIPT"
+# the expected setting path. The content is also validated by the null check
+# script described above, which can potentially return a non-zero status code.
+jq --null-input --from-file /settings.json.jq | tee $SETTINGS_PATH | jq "$NULL_CHECK_SCRIPT"
 
 # Start the gunicorn service with an empty environment. This is an attempt to
 # minimize damage in the event code execution is obtained through the service
