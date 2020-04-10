f8907cb8af17e87cbc587f7fa6a01b6b8738c63f
Brahm Lower <bplower@gmail.com>
2020-04-10 16:40:31 -0700

    split jq rendering commands out into their own shell script
    This makes the templating logic a little more self contained, and makes it easier for the init script to template multiple json files

--- example-service/init_container.sh
+++ example-service/init_container.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env sh
-
-# Set abort on error. This will cause the script to fail if the jq templating
-# process fails, thus preventing the service from starting with a potentially
-# invalid configuration.
 set -e
+
 SETTINGS_PATH=/settings.json
+TEMPLATE_PATH=/settings.json.jq
 
-# This is a jq script that searches through a given structure for null values
-# and prints a human readable error for each occurence. Causes jq to exit with
-# status code 5 if there are errors
-NULL_CHECK_SCRIPT='[ path(..|select(type=="null")) | "Missing or invalid value for config: .\( join(".") )\n" ] | if length > 0 then (join("") | halt_error) else empty end'
+# If we had multiple config files that needed to be rendered, we could do that
+# here by calling jq-render several times like:
+#   jq-render application.json.jq application.json
+#   jq-render database.json.jq database.json
+jq-render $TEMPLATE_PATH $SETTINGS_PATH
 
-# Process the settings template file (the *.jq file), writing the result to
-# the expected setting path. The content is also validated by the null check
-# script described above, which can potentially return a non-zero status code.
-jq --null-input --from-file /settings.json.jq | tee $SETTINGS_PATH | jq "$NULL_CHECK_SCRIPT"
-
-# Start the gunicorn service with an empty environment. This is an attempt to
-# minimize damage in the event code execution is obtained through the service
+# Start the gunicorn service with an empty environment using `env -1`. This is
+# an attempt to minimize damage in the event code execution is obtained through
+# the service
 env -i gunicorn --bind 0.0.0.0:8000 "example_service:build_app('$SETTINGS_PATH')"
